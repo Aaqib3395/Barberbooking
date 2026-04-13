2faaf33d15dcb2906892786f7c17b13424dc940d
Aaqib3395 <66707721+Aaqib3395@users.noreply.github.com>
2026-04-13 23:06:13 +0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({ baseURL: 'https://talented-imagination-production-dec0.up.railway.app/api' });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
